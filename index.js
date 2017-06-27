a628b7cafc843e0abe9781c6a9004808ff929219
andrewmarino1 <andrew.m.marino@gmail.com>
2017-06-27 14:34:12 -0400

    Add files via upload
    updated: views working for each page with default layout. Things needed: css on local server, working nav bar on local server, connection to heroku, gitignore file.

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const Path = require('path');
-const Hapi = require('hapi');
-const Inert = require('inert');
- 
-const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, 'knox')
-            }
+var hapi = require('hapi');
+
+// Create hapi server instance
+var server = new hapi.Server();
+
+// add connection parameters
+server.connection({  
+    host: 'localhost',
+    port: 3000
+});
+
+server.register(require('vision'))
+
+server.views({  
+    engines: {
+        html: require('handlebars')
+    },
+    path: 'server_views',
+    layoutPath: 'server_views/layout',
+    layout: 'default'
+});
+
+// create your routes, currently it's just one
+var routes = [  
+    {
+        method: 'GET',
+        path: '/index.html',
+        handler: function(request, reply) {
+            return reply.view('index');
+        }
+    },
+	 {
+        method: 'GET',
+        path: '/about.html',
+        handler: function(request, reply) {
+            return reply.view('about');
+        }
+    },
+     {
+        method: 'GET',
+        path: '/contact.html',
+        handler: function(request, reply) {
+            return reply.view('contact');
         }
     }
-});
-server.connection({ port: 3000 });
- 
-server.register(Inert, () => {});
- 
-server.route({
-    method: 'GET',
-    path: '/knox/index.html',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
-    }
-});
- 
-server.start((err) => {
- 
-    if (err) {
-        throw err;
-    }
- 
-    console.log('Server running at:', server.info.uri);
+];
+
+// tell your server about the defined routes
+server.route(routes);
+
+// Start the server
+server.start(function() {  
+    // Log to the console the host and port info
+    console.log('Server started at: ' + server.info.uri);
 });
